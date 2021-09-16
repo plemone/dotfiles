fab577913de9e785f85001cd35184da5deee346f
tanvirtin <tanvir.tinz@gmail.com>
2021-09-16 08:39:08 -0400

    08:39:08

--- nvim/lua/config/autocomplete.lua
+++ nvim/lua/config/autocomplete.lua
@@ -1,15 +1,5 @@
 local cmp = require('cmp')
-local compare = require('cmp.config.compare')
 local lspkind = require('lspkind')
-
-local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
-end
 
 cmp.setup({
     formatting = {
@@ -32,57 +22,9 @@ cmp.setup({
     },
     min_length = 0,
     mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if vim.fn['UltiSnips#CanJumpBackwards']() == 1 then
-                vim.fn.feedkeys(t('<C-R>=UltiSnips#JumpBackwards()<CR>'))
-            elseif vim.fn.pumvisible() == 1 then
-                vim.fn.feedkeys(t('<C-p>'), 'n')
-            else
-                vim.fn.feedkeys(t('<S-tab>'), 'n')
-            end
-        end, {
-            'i',
-            's',
-        }),
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if vim.fn['UltiSnips#CanJumpForwards']() == 1 then
-                vim.fn.feedkeys(t('<esc>:call UltiSnips#JumpForwards()<CR>'))
-            elseif vim.fn.pumvisible() == 1 then
-                vim.fn.feedkeys(t('<C-n>'), 'n')
-            else
-                vim.fn.feedkeys(t('<tab>'), 'n')
-            end
-        end, {
-            'i',
-            's',
-        }),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = false,
-        }),
-        ['<C-Space>'] = cmp.mapping(function(fallback)
-            if vim.fn.pumvisible() == 1 then
-                if vim.fn.complete_info()['selected'] ~= -1 then
-                    if vim.fn['UltiSnips#CanExpandSnippet']() == 1 then
-                        return vim.fn.feedkeys(t('<C-R>=UltiSnips#ExpandSnippet()<CR>'))
-                    else
-                        vim.fn.feedkeys(t('<cr>'), 'n')
-                    end
-                else
-                    vim.fn.feedkeys(t('<C-e>'), 'n')
-                end
-            else
-                cmp.complete()
-            end
-        end, {
-            'i',
-            's',
-        }),
+        mapping = {
+            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        },
     },
     sources = {
         { name = 'cmp_tabnine' },
@@ -92,13 +34,6 @@ cmp.setup({
         { name = 'nvim_lua' },
         { name = 'path' },
         { name = 'calc' },
-    },
-    sorting = {
-        priority_weight = 1.5,
-        comparators = {
-            compare.score,
-            compare.offset,
-        },
     },
 })
 
